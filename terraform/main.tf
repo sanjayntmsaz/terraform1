832694a97d3b083ad51b129ae4c0e69e2a0d7bba
sanjay <sanjayntms@yahoo.com>
2021-12-28 09:36:41 +0000

    t3

--- terraform/main.tf
+++ terraform/main.tf
@@ -88,34 +88,3 @@ output "public_ip_address" {
 data "azurerm_client_config" "current" {
 }
 
-output "account_id" {
-  value = data.azurerm_client_config.current.client_id
-}
-resource "azurerm_key_vault" "example" {
-  name                        = "ntmskeyvault2"
-  location                    = azurerm_resource_group.example.location
-  resource_group_name         = azurerm_resource_group.example.name
-  enabled_for_disk_encryption = true
-  tenant_id                   = data.azurerm_client_config.current.tenant_id
-  soft_delete_retention_days  = 7
-  purge_protection_enabled    = false
-
-  sku_name = "standard"
-
-  access_policy {
-    tenant_id = data.azurerm_client_config.current.tenant_id
-    object_id = data.azurerm_client_config.current.object_id
-
-    key_permissions = [
-      "Get",
-    ]
-
-    secret_permissions = [
-      "Get",
-    ]
-
-    storage_permissions = [
-      "Get",
-    ]
-  }
-}
