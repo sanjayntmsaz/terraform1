45df7002bafe883852e2cbf389ce58461370e0ea
sanjay <sanjayntms@yahoo.com>
2021-12-28 11:13:54 +0000

    t3

--- terraform/main.tf
+++ terraform/main.tf
@@ -28,6 +28,12 @@ resource "azurerm_subnet" "example" {
   virtual_network_name = azurerm_virtual_network.example.name
   address_prefixes     = ["10.0.2.0/24"]
 }
+resource "azurerm_subnet" "example" {
+  name                 = "web"
+  resource_group_name  = azurerm_resource_group.example.name
+  virtual_network_name = azurerm_virtual_network.example.name
+  address_prefixes     = ["10.0.3.0/24"]
+}
 
 resource "azurerm_network_interface" "example" {
   name                = "example-nic"
