96a1737c7cbfce4fbd8b7f04fdd62e6e5a92fb42
Sanjay NTMS <sanjayntmsaz@outlook.com>
2022-11-12 06:58:41 +0000

    4

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,14 +1,14 @@
 terraform {
-    
+
   backend "azurerm" {
-    resource_group_name   = "tf-RG"
-    storage_account_name  = "ntmstfsa"
-    container_name        = "tfstate"
-    key                   = "terraform.tfstate"
+    resource_group_name  = "tf-RG"
+    storage_account_name = "ntmstfsa"
+    container_name       = "tfstate"
+    key                  = "terraform.tfstate"
   }
   required_providers {
     azurerm = {
-      source = "hashicorp/azurerm"
+      source  = "hashicorp/azurerm"
       version = "=3.0.1"
     }
   }
