c71b309abe6801288c258662f2aea1c1adee0233
sanjayntmsaz <114060293+sanjayntmsaz@users.noreply.github.com>
2022-12-10 12:59:54 +0000

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -48,7 +48,7 @@ resource "azurerm_network_interface" "example" {
   resource_group_name = azurerm_resource_group.example.name
 
   ip_configuration {
-    name                          = "internal"
+    name                          = "internal1"
     subnet_id                     = azurerm_subnet.example.id
     private_ip_address_allocation = "Dynamic"
     public_ip_address_id          = azurerm_public_ip.example.id
